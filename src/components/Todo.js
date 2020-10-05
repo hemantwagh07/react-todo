313fd7b5b825a83a0fcfff3c497e82235a5368cd
Hemant Wagh <hemantwagh07@gmail.com>
2020-10-06 00:24:30 +0530

    complete task functionality
    complete task functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,11 +8,28 @@ export default function Todo({ text, setTodos, todos, todo }) {
             })
         );
     }
+
+    const completeHandler = () => {
+        setTodos(
+            todos.map((el) => {
+                if (el.id === todo.id) {
+                    return {
+                        ...el, completed: !el.completed
+                    }
+                }
+                return el;
+            })
+        )
+    }
     return (
         <div className='todo'>
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
-            <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
+            <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
         </div>
     )
 }
