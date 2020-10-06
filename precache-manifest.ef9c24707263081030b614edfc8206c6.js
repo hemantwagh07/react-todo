ee204f865d884ca248595ef6a0ddbd1710dff98d
Hemant Wagh <hemantwagh07@gmail.com>
2020-10-06 12:06:22 +0530

    Updates

---
+++ precache-manifest.ef9c24707263081030b614edfc8206c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41496ce7bad1982cccb1f3ded93d97cd",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "b73a74189fc5b4f5e360",
+    "url": "/react-todo/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "fda7aeacd84abf9db942",
+    "url": "/react-todo/static/js/2.5de09e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.5de09e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73a74189fc5b4f5e360",
+    "url": "/react-todo/static/js/main.6540c1ab.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
